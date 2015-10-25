b45d3f0b2fbd2cfce7b9cd8f13d0d9d89ffe8084
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2015-10-25 13:42:48 +0100

    implemented map-set test

--- 03-yield/test/features/map-set.js
+++ 03-yield/test/features/map-set.js
@@ -27,6 +27,23 @@ export default function() {
     myMap.get('second').should.eql(2)
   })
 
-  it.skip('should show Map/Set behavior in ES6', () => {
+  it('should show Map/Set behavior in ES6', () => {
+    const mySet = new Set()
+    mySet
+      .add(1)
+      .add(2)
+      .add(3)
+
+    mySet.has(1).should.be.true
+    mySet.has(2).should.be.true
+    mySet.has(3).should.be.true
+    mySet.has(4).should.be.false
+
+    const myMap = new Map()
+    myMap.set('Hello', "World!")
+    myMap.set('second', 2)
+
+    myMap.get('Hello').should.eql('World!')
+    myMap.get('second').should.eql(2)
   })
 }
