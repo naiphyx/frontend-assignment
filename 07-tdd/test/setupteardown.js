a568316abcd6a6a65de3f361347a217d7f1a2b2c
Viktoria Steiner <vsteiner.mmt-b2014@fh-salzburg.ac.at>
2015-11-27 21:37:28 +0100

    feat: add setup and teardown test

---
+++ 07-tdd/test/setupteardown.js
@@ -0,0 +1,27 @@
+var should = require('should')
+var createDog = require('../src/setupteardown.js')
+
+describe('setup', function() {
+
+	var dog;
+
+  before(function() {
+  	console.log("before test: create dog")
+  	dog = createDog("Franzi")
+  })
+
+  after(function() {
+    console.log("after test: destroy dog")
+    dog = null;
+  })
+
+  it('dog should have name', function() {
+  	dog.should.have.property('name')
+  	dog.name.should.be.a.String()
+  })
+
+  it('dog should make woof sound', function() {
+  	should(dog.makeSound()).eql("Woof")
+  })
+
+})
